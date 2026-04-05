c924d41d20994beec1457d5205c2065e2c85a317
haryoiro <mizusecocolte@gmail.com>
2026-04-05 23:53:47 +0900

    📦 foxus: バージョンを 0.0.1 に変更

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "PyokotifyCore", targets: ["PyokotifyCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/haryoiro/foxus", from: "0.2.0"),
+        .package(url: "https://github.com/haryoiro/foxus", from: "0.0.1"),
     ],
     targets: [
         // メインの実行ファイル
